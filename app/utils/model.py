59d5569c6e539b080347c7b3ee81795ef6821165
Humairajahan <humairaazim45@gmail.com>
2023-07-04 17:38:59 +0600

    updated base URL

--- app/utils/model.py
+++ app/utils/model.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, HttpUrl
 
 
 class URLRequest(BaseModel):
-    baseurl: HttpUrl = "http://montypy.ai"
+    baseurl: HttpUrl = "http://127.0.0.1:5000"
     longurl: HttpUrl = "https://pythonprogramminglanguage.com/randon-numbers"
